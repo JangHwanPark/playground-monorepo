963ba8cf07995ec7937c320df7f13641fc0b9fef
JangHwanPark <wkd5878@gmail.com>
2026-03-12 16:20:19 +0900

    자바스크립트(알고리즘: 인접 리스트 BFS) - 재활훈련

--- apps/js-playground/bfs/exam1.js
+++ apps/js-playground/bfs/exam1.js
@@ -1,39 +1,51 @@
-const grid = [
-  ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
+// === 입력 ===
+// 노드: 0 ~ 4
+// 간선: 0-1, 0-2, 1-3, 2-4
+const graph = [
+  [1, 2],    // 0번과 연결된 노드들
+  [0, 3],    // 1번과 연결된 노드들
+  [0, 4],    // 2번과 연결된 노드들
+  [1],       // 3번과 연결된 노드들
+  [2]        // 4번과 연결된 노드들
 ];
 
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
+// 0번 노드는 1번노드, 2번 노드와 연결되어있음.
+console.log("graph[0]:", graph[0]);
+// 1번 노드는 0번노드, 3번 노드와 연결되어있음.
+console.log("graph[1]:", graph[1]);
+console.log("graph[2]:", graph[2]);
+console.log("graph[3]:", graph[3]);
+console.log("graph[4]:", graph[4], "\n")
 
-const n = grid.length;
-const m = grid[0].length;
-let count = 0;
+const start = 0;
+const city = ["서울","부산","대구","광주","대전"];
 
-for (let row = 0; row < n; row++) {
-  for (let col = 0; col < m; col++) {
-    if (grid[row][col] === "1") {
-      count++;
-      const queue = [[row, col]];
-      grid[row][col] = 0;
+const visited = new Set([start]);
+const queue = [start];
+const result = [];
+const valueResult = [];
+console.log('초기화:',visited, queue, result, valueResult);
 
-      while (queue.length > 0) {
-        const [x, y] = queue.shift();
+while (queue.length > 0) {
+  const node = queue.shift();
+  console.log("방문 노드:", node);
+  console.log("방문한 도시:", city[node]);
+  console.log("방문한 노드의 인접 리스트:", graph[node]);
 
-        for (let i = 0; i < 4; i++) {
-          const nx = x + dx[i];
-          const ny = y + dy[i];
+  result.push(node);
+  console.log("방문처리:", result);
 
-          if (nx >= 0 && ny >= 0 && nx < n && ny < m && grid[nx][ny] === "1") {
-            grid[nx][ny] = 0;
-            queue.push([nx, ny]);
-          }
-        }
-      }
+  valueResult.push(city[node]);
+  console.log('방문한 값 저장:', valueResult);
+
+  console.log("\nnode 인접노드 순회")
+  for (const next of graph[node]) {
+    if (!visited.has(next)) {
+      visited.add(next);
+      queue.push(next);
+      console.log("큐에 추가된 노드(next):", next);
     }
   }
 }
 
-console.log(count);
+console.log('방문순서:', result);
